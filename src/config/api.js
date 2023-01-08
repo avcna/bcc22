71d0fdf387ba5d3d2d181e7f83ccd9b5b3d9737f
avcna <avocadobanana33@gmail.com>
2023-01-08 10:49:45 +0700

    remove headers

--- src/config/api.js
+++ src/config/api.js
@@ -2,9 +2,4 @@ import axios from "axios";
 
 export const petlinkAPI = axios.create({
   baseURL: "https://web-service-petlink.onrender.com/",
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
-    "Content-Type": "application/json",
-  },
 });
